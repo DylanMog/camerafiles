b2e3d34dbf7a35da3c8c4c4e67a64fccfcc82b9b
DylanMog <dyluu.m@gmail.com>
2015-05-11 12:24:20 -0300

    cambio
    cambio

---
+++ index.js
@@ -0,0 +1,29 @@
+// JavaScript Document
+
+
+
+
+function sacaFoto(){
+navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+    destinationType: Camera.DestinationType.FILE_URI
+});
+}
+
+
+function onSuccess(ruta) {
+    var image = document.getElementById('myImage');
+    image.src = ruta;
+	
+	var parrafo = document.getElementById('texto');
+	parrafo.innerHTML = ruta;
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
+
+
+
+
+	
+	
